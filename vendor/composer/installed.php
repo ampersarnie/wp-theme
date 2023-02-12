6d4e6e372a597cf065b9bbe1b5d130446c4488d3
CIDeployment <email>
2023-02-12 00:02:42 +0000

    ci: deploy 2db3718fae149f7b1cab5afed80712e7b8bebc15

---
+++ vendor/composer/installed.php
@@ -0,0 +1,44 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'wordpress-theme',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2db3718fae149f7b1cab5afed80712e7b8bebc15',
+        'name' => 'ampersarnie/wp-theme',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'ampersarnie/wp-theme' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'wordpress-theme',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2db3718fae149f7b1cab5afed80712e7b8bebc15',
+            'dev_requirement' => false,
+        ),
+        'composer/installers' => array(
+            'pretty_version' => 'v1.12.0',
+            'version' => '1.12.0.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'reference' => 'd20a64ed3c94748397ff5973488761b22f6d3f19',
+            'dev_requirement' => false,
+        ),
+        'roundcube/plugin-installer' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'shama/baton' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+    ),
+);
