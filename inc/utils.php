949d04ef5e4975faf4ddf9dfbdf1c2869cc7572e
Paul Taylor <paul.taylor@hey.com>
2023-02-26 14:42:48 +0000

    chore: update with PSR12 standards

--- inc/utils.php
+++ inc/utils.php
@@ -9,16 +9,15 @@ namespace Ampersarnie\WP\Theme;
  * @author Paul Taylor <paul.taylor@hey.com>
  * @return array
  */
-function get_stored_posts(): array {
-	global $posts;
+function get_stored_posts(): array
+{
+    $post_args = [
+        'nopaging' => true, // phpcs:ignore WordPressVIPMinimum.Performance.NoPaging.nopaging_nopaging
+    ];
 
-	$post_args = [
-		'nopaging' => true, // phpcs:ignore WordPressVIPMinimum.Performance.NoPaging.nopaging_nopaging
-	];
+    $stored_posts = \get_posts($post_args);
 
-	$stored_posts = \get_posts( $post_args );
-
-	return $stored_posts;
+    return $stored_posts;
 }
 
 /**
@@ -28,14 +27,15 @@ function get_stored_posts(): array {
  * @throws \ErrorException Throws when not in the the loop.
  * @return string
  */
-function get_formatted_date(): string {
-	global $post;
+function get_formatted_date(): string
+{
+    global $post;
 
-	if ( ! in_the_loop() ) {
-		throw new \ErrorException( 'Must call get_formatted_date() from the loop.' );
-	}
+    if (!in_the_loop()) {
+        throw new \ErrorException('Must call get_formatted_date() from the loop.');
+    }
 
-	return \get_the_date( '', $post );
+    return \get_the_date('', $post);
 }
 
 /**
@@ -45,10 +45,11 @@ function get_formatted_date(): string {
  * @throws \ErrorException Throws when not in the the loop.
  * @return array
  */
-function get_post_categories(): array {
-	global $post;
+function get_post_categories(): array
+{
+    global $post;
 
-	return \wp_get_post_categories( $post->ID, [ 'fields' => 'names' ] );
+    return \wp_get_post_categories($post->ID, [ 'fields' => 'names' ]);
 }
 
 /**
@@ -60,22 +61,23 @@ function get_post_categories(): array {
  * @throws \ErrorException Throws when not in the the loop.
  * @return void|string
  */
-function get_datetime_html( string $format = '', bool $print = false ) {
-	global $post;
+function get_datetime_html(string $format = '', bool $print = false)
+{
+    global $post;
 
-	if ( ! in_the_loop() ) {
-		throw new \ErrorException( 'Must call get_datetime_html() from the loop.' );
-	}
+    if (! in_the_loop()) {
+        throw new \ErrorException('Must call get_datetime_html() from the loop.');
+    }
 
-	$datetime_value = \get_post_time( 'Y-m-dTH:i:sZ', true, $post, false );
-	$formatted      = \get_the_date( $format, $post );
+    $datetime_value = \get_post_time('Y-m-dTH:i:sZ', true, $post, false);
+    $formatted      = \get_the_date($format, $post);
 
-	if ( $print ) {
-		printf( '<time datetime="%s">%s</time>', esc_attr( $datetime_value ), esc_html( $formatted ) );
-		return;
-	}
+    if ($print) {
+        printf('<time datetime="%s">%s</time>', esc_attr($datetime_value), esc_html($formatted));
+        return;
+    }
 
-	return sprintf( '<time datetime="%s">%s</time>', esc_attr( $datetime_value ), esc_html( $formatted ) );
+    return sprintf('<time datetime="%s">%s</time>', esc_attr($datetime_value), esc_html($formatted));
 }
 /**
  * Get an array of available colours.
@@ -84,29 +86,30 @@ function get_datetime_html( string $format = '', bool $print = false ) {
  * @param string $category The category or key of the requested colours.
  * @return array
  */
-function get_category_colors( string $category ): array {
-	$category = strtolower( $category );
+function get_category_colors(string $category): array
+{
+    $category = strtolower($category);
 
-	$colors = [
-		'default-dark'  => [
-			'text'  => 'var(--color-category-text-default)',
-			'color' => 'var(--color-category-default)',
-		],
-		'default-light' => [
-			'text'  => 'var(--color-category-text-default)',
-			'color' => 'var(--color-category-default)',
-		],
-		'uncategorized' => [
-			'text'  => 'var(--color-font-base)',
-			'color' => '#e9ab00',
-		],
-	];
+    $colors = [
+        'default-dark'  => [
+            'text'  => 'var(--color-category-text-default)',
+            'color' => 'var(--color-category-default)',
+        ],
+        'default-light' => [
+            'text'  => 'var(--color-category-text-default)',
+            'color' => 'var(--color-category-default)',
+        ],
+        'uncategorized' => [
+            'text'  => 'var(--color-font-base)',
+            'color' => '#e9ab00',
+        ],
+    ];
 
-	if ( \array_key_exists( $category, $colors ) ) {
-		return $colors[ $category ];
-	}
+    if (\array_key_exists($category, $colors)) {
+        return $colors[ $category ];
+    }
 
-	return $colors['default-dark'];
+    return $colors['default-dark'];
 }
 
 /**
@@ -116,10 +119,11 @@ function get_category_colors( string $category ): array {
  * @param string $category The requested category.
  * @return string
  */
-function get_category_color_vars( string $category ): string {
-	$colors = get_category_colors( $category );
+function get_category_color_vars(string $category): string
+{
+    $colors = get_category_colors($category);
 
-	return color_var_string( $colors['color'], $colors['text'] );
+    return color_var_string($colors['color'], $colors['text']);
 }
 
 /**
@@ -130,23 +134,24 @@ function get_category_color_vars( string $category ): string {
  * @param string $text The text colour.
  * @return string
  */
-function color_var_string( $color = null, $text = null ): string {
-	$output = '';
+function color_var_string($color = null, $text = null): string
+{
+    $output = '';
 
-	if ( is_string( $color ) ) {
-		$output = sprintf(
-			'--color-category:%s;', 
-			$color
-		);
-	}
+    if (is_string($color)) {
+        $output = sprintf(
+            '--color-category:%s;',
+            $color
+        );
+    }
 
-	if ( is_string( $text ) ) {
-		$output = sprintf(
-			'%s--color-category-text:%s;',
-			$output,
-			$text
-		);
-	}
+    if (is_string($text)) {
+        $output = sprintf(
+            '%s--color-category-text:%s;',
+            $output,
+            $text
+        );
+    }
 
-	return $output;
+    return $output;
 }
